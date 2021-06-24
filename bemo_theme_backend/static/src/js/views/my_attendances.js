b724b3287175808455df2427c8ce2c2d1e386113
Hoàng Việt Hùng <hoangviet.hung.009@gmail.com>
2021-06-24 18:13:59 +0700

    update

--- bemo_theme_backend/static/src/js/views/my_attendances.js
+++
@@ -1,9 +0,0 @@
-odoo.define("bemo_theme_backend.my_attendances", function (require) {
-	"use strict";
-
-	var core = require("web.core");
-	var ajax = require("web.ajax");
-	var qweb = core.qweb;
-
-	ajax.loadXML("/bemo_theme_backend/static/src/xml/attendance.xml", qweb);
-});
